666d0cee9537919dd2715289812a24dd70249bf8
adil <adilahmmed1444@gmail.com>
2021-09-01 16:27:58 +0530

    successfully made the changing of colors

--- script.js
+++ script.js
@@ -4,18 +4,60 @@ class players{
     constructor(color){
         this.color=color
         this.position=0
+        this.prevposition=0
+        this.letter= this.setletter()
+    }
+    setposition=(n)=>{
+        this.prevposition=this.position
+        this.position=this.position+n
+    }
+    setletter=()=>{
+        let [a]=this.color
+        return(a[0])
     }
 }
 
 let player1= new players("red")
+let player2= new players("green")
+let player3= new players("blue")
+let player4= new players("yellow")
+let playerslist=[player1,player2,player3,player4]
+let n=0
+let nowplay=playerslist[n]
+
 
 const spin=()=>{
+    console.log(nowplay);
     let random=Math.floor(Math.random()*6 )+1 
     console.log(random);
-    if(random===1){
-        
-    }
+    nowplay.setposition(random)
+    console.log(nowplay);
+    move(nowplay)        
+
+    
+    updatenowplay()
 }
 
 
-console.log(player1);
+
+
+const move=(nowplay)=>{
+    document.querySelector(`.num${nowplay.position}`).innerHTML=document.querySelector(`.num${nowplay.position}`).innerHTML+`<p2 class="${nowplay.color}" style="background-color: ${nowplay.color};">  ${nowplay.letter}</p2>`
+    if(nowplay.prevposition!==0){
+        console.log( document.querySelector(`.num${nowplay.prevposition}`).childNodes[3]);
+        document.querySelector(`.num${nowplay.prevposition}`).childNodes[3].remove()
+        // document.querySelector(`.num${nowplay.prevposition}`).innerHTML=`<p2style="background-color: black;>k </p2>`
+    }
+    
+    // document.querySelector(`.num${position}`).style.backgroundColor=color
+
+}
+
+
+const updatenowplay=()=>{
+    n++
+    if(n===4){
+        n=0
+    }
+    nowplay=playerslist[n]
+}
